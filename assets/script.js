eeb1701cf5caa6ce7aa96e14808c7e1e79ce4637
Tamar <mischegoss@gmail.com>
2021-05-15 18:18:54 -0400

    Refine prototype

--- assets/script.js
+++ assets/script.js
@@ -1,65 +1,60 @@
-// Array of possible choices; Shows planning for use "Git" and "git" w/o introducing string methods
-var reviewSections = ["Git", "git", "HTML", "html", "CSS", "Javascript", "JavaScript"]
+/* Key Topic variables and array */
+var topics = ["Git", "git", "GitHub", "HTML", "CSS"]
+var highlighted = "";
+var searchTopic;
 
-// Shows variable can get value later
-var highlighted;
-
-// function declaration 
-function sectionPicker() {
-  
-    // Window prompts. Needed to demo interactivity 
-    var selectedSection = prompt("What section would you like to review: Git, HTML, CSS, JavaScript");
-    // Need for loop
-    for (var x =0; x < reviewSections.length; x++) {
-        // Array index
-        if (selectedSection === reviewSections[x]) {
-          // Reinforces zero indexing of arrays
-          if (x <=1) {
-            // Shows calling function with parameter; Function provided
-             highlightSection("git-section")
-             // Shows return use case
-             return
-          } 
-        }
-    }
-
-    // Shows need for error messaging
-    alert("Oh no, try again");
-    // Shows calling function in function; Can be eliminated if out of scope
-    sectionPicker();
+/*Key Topic: Functions */
+function topicChecker() {
+/* Worth Knowing: Window Functions */
+searchTopic = prompt("What topic would you like use to search for? For testing use Git or HTML")
+/* Key Topic: for Loop) */
+for (var x=0; x< topics.length; x++) {
+  if (searchTopic === topics[x]) {
+    selectSection()
+    /* Worth Knowing: return usage */
+    return;
+  } else {
+    alert("Topic is not covered")
+    return;
+  }
+ }
 }
 
-// This is the helper function. PROVIDED for students
+function selectSection() {
+    if (searchTopic === "Git") {
+      /* Worth Knowing: Functions can take parameters */
+      highlightSection("git-section");
+      return;
+     
+    // TODO: These need to be expanded to include topics in array
+    } else if (selectionPrompt === "HTML" || selectionPrompt === "Semantic HTML") {
+        highlightSection("html-section");
+        return;
+
+    } else if (selectionPrompt === "CSS") {
+        highlightSection("css-section");
+        return;
+
+    } else if (selectionPrompt === "JavaScript") {
+        highlightSection("javascript-section");
+        return;
+
+    } else if (selectionPrompt === "Deployment") {
+        highlightSection("deployment-section");
+        return;
+    /* Worth Knowing: Error Handling intro */
+    } else {
+      alert("Oh no! Something went wrong")
+    }
+}
+
+// This helper function is provided for students
 function highlightSection(target) {
     highlighted = document.getElementById(target);
-     // This is just a bonus to show JavaScript is awesome
     highlighted.scrollIntoView({behavior: "smooth"});
-    // Changes color
     highlighted.style.backgroundColor = "yellow";
 }
 
-// Calling function
-sectionPicker();
-
-
-/************************************Skills Covered ****************************/
-/* Skills used
- * Array
- * Array Zero Index
- * For Loop
- * If/Else
- * Declaring a Function
- * Calling a Function
- * Declaring variable with var
- * Window alerts and prompts
- * return
- */
-
-/* Skills that are shown PROVIDED)
- * document.getElementbyId()
- * Style property
- * scrollintoView()
- */
-/********************************************************************************** */
-
-
+// This button code can be provided to students
+var button = document.getElementById("button")
+button.addEventListener("click", topicChecker)
